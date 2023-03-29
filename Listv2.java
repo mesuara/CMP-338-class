adb5185c2820eedb812d1a302c0925b42c4a7589
mesuara <37057206+mesuara@users.noreply.github.com>
2023-03-29 00:46:03 +0000

    fixed the variable on the listv2

--- Listv2.java
+++ Listv2.java
@@ -1,8 +1,8 @@
 public class Listv2{
   Node tail = new Node(0,null);
   Node head = new Node(0, tail);
-  private Node head = dum1;
-  private Node prev = dum1;
+  private Node prev = head;
+  private Node curr = head;
   
   
 }
